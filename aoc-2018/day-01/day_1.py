bd263e0e121d93618b9b6f2f6aa8fc95638e7fdc
Bendik Samseth <b.samseth@gmail.com>
2018-12-01 19:14:10 +0100

    Solve part two.

--- aoc-2018/day-01/day_1.py
+++ aoc-2018/day-01/day_1.py
@@ -1,5 +1,18 @@
-with open('input1.txt', 'r') as f:
-    freq = 0
-    for line in f:
-        freq += eval(line.strip())
-    print('Final frequency:', freq)
+import numpy as np
+from itertools import count
+
+# Part one
+data = np.loadtxt("input1.txt", dtype=int)
+print("Final frequency:", np.sum(data))
+
+# Part two
+seen = set([0])
+freq = 0
+for index in count():
+    freq += data[index % len(data)]
+    if freq in seen:
+        break
+    seen.add(freq)
+
+print("First frequency reached twice:", freq)
+print("Found in {} iterations".format(index))
