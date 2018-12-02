6e28e12ca9656ec1b174053d3cdb2d3fadaa9a3b
Bendik Samseth <b.samseth@gmail.com>
2018-12-02 17:27:29 +0100

    Solve day 2 part 2.

--- aoc-2018/day-02/day_2.py
+++ aoc-2018/day-02/day_2.py
@@ -1,8 +1,9 @@
 from collections import Counter
 
 with open('input1.txt', 'r') as f:
-    ids = f.read().split('\n')
+    ids = f.read().strip().split('\n')
 
+# Part 1
 double_count = 0
 tripple_count = 0
 for i in ids:
@@ -11,4 +12,26 @@ for i in ids:
     tripple_count += any(count == 3 for count in counts.values())
 
 print('Checksum:', double_count * tripple_count)
+
+# Part 2.
+# The naive search (for each id, check all others for a match) is O(N^2).
+# Better is O(M * N) = O(N), with M being the (max) length of the ids:
+
+# If not all ids are the same length, use the maximum. Not an issue here.
+assert all(len(i) == len(ids[0]) for i in ids)
+
+# Remove the j-th char from each id and check for any duplicates.
+done = False
+for j in range(len(ids[0])):
+    seen = set()
+    for i in (id_i[:j] + id_i[j+1:] for id_i in ids):
+        if i in seen:
+            done = True
+            break
+        seen.add(i)
+
+    if done:
+        break
+
+print('Common chracters:', i)
 
