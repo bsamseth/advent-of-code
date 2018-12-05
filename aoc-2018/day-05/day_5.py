e7076e210181a19d447f945fce1a9f89abf0f24d
Bendik Samseth <b.samseth@gmail.com>
2018-12-05 17:35:37 +0100

    Refactor day 5.

--- aoc-2018/day-05/day_5.py
+++ aoc-2018/day-05/day_5.py
@@ -1,33 +1,26 @@
 from string import ascii_lowercase
-from mpi4py import MPI
 
 with open("input.txt", "r") as f:
     data = f.read().strip()
 
-
 # Part 1
 def react(polymer):
-    i = 0
-    while i < len(polymer) - 1:
-
-        # Lowercase - uppercase differs by 32 in ASCII
-        if abs(ord(polymer[i]) - ord(polymer[i + 1])) == 32:
-            polymer = polymer[:i] + polymer[i + 2 :]
-            i = max(0, i - 1)  # Backtrack one step to account for new possible match.
-
+    result = []
+    for unit in polymer:
+        if result and unit.swapcase() == result[-1]:
+            result.pop()
         else:
-            i += 1
+            result.append(unit)
+    return len(result)
 
-    return len(polymer)
 
 print("Polymer contains {} units after fully reacting.".format(react(data)))
 
 
 # Part 2
-shortest = float('inf')
+shortest = float("inf")
 for char in ascii_lowercase:
     removed = data.replace(char, "").replace(char.upper(), "")
     shortest = min(shortest, react(removed))
 
 print("Shortest possible polymer contains {} units.".format(shortest))
-
