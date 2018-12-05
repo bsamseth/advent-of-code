7d3488e6ade0405d6c5072e5c0e94285350e6598
Bendik Samseth <b.samseth@gmail.com>
2018-12-05 16:15:22 +0100

    Solve day 5.

--- aoc-2018/day-05/day_5.py
+++ aoc-2018/day-05/day_5.py
@@ -1,17 +1,33 @@
+from string import ascii_lowercase
+from mpi4py import MPI
+
 with open("input.txt", "r") as f:
     data = f.read().strip()
 
-i = 0
-while i < len(data) - 1:
 
-    # Lowercase - uppercase differs by 32 in ASCII
-    if abs(ord(data[i]) - ord(data[i + 1])) == 32:
-        data = data[:i] + data[i + 2 :]
-        i = max(0, i - 1)  # Backtrack one step to account for new possible match.
+# Part 1
+def react(polymer):
+    i = 0
+    while i < len(polymer) - 1:
 
-    else:
-        i += 1
+        # Lowercase - uppercase differs by 32 in ASCII
+        if abs(ord(polymer[i]) - ord(polymer[i + 1])) == 32:
+            polymer = polymer[:i] + polymer[i + 2 :]
+            i = max(0, i - 1)  # Backtrack one step to account for new possible match.
 
-    print("Progress: {:05d}".format(i), end="\r")
+        else:
+            i += 1
 
-print("Polymer contains {} units after fully reacting.".format(len(data)))
+    return len(polymer)
+
+print("Polymer contains {} units after fully reacting.".format(react(data)))
+
+
+# Part 2
+shortest = float('inf')
+for char in ascii_lowercase:
+    removed = data.replace(char, "").replace(char.upper(), "")
+    shortest = min(shortest, react(removed))
+
+print("Shortest possible polymer contains {} units.".format(shortest))
+
