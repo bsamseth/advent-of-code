333955629a3f85b7e8042baa2658b8fedd4ffd70
Bendik Samseth <b.samseth@gmail.com>
2018-12-05 17:16:36 +0100

    Update get_data.py to also make a README.
    The content of the description for the given day will
    be fetched and made into markdown for display on github.

--- get_data.py
+++ get_data.py
@@ -1,11 +1,35 @@
 """
-Fetch data for a given day and year and output it to a text file.
+Fetch data and description for a given day and year and output it given folder.
 
 By default the current day is attempted.
 """
-from aocd import get_data
+# from aocd import get_data
 import argparse
 import datetime
+import os
+import sys
+import requests
+from bs4 import BeautifulSoup
+from tomd import Tomd
+
+
+def get_content(year, day, path='', session_key=None):
+    url = "https://adventofcode.com/{}/day/{}".format(year, day)
+    if path:
+        url += '/' + path
+
+    resp = requests.get(url, headers={'cookie':'session={};'.format(session_key)})
+
+    if not resp.ok:
+        raise Exception("Bad request: {}".format(resp.status_code))
+
+    if path:
+        return resp.text.strip()
+
+    soup = BeautifulSoup(resp.text, 'html.parser')
+    return Tomd(str(soup.main)).markdown
+
+
 
 parser = argparse.ArgumentParser(description=__doc__)
 
@@ -23,9 +47,37 @@ parser.add_argument(
     default=datetime.date.today().day,
     help="Which day to fetch data for.",
 )
+parser.add_argument(
+    "--session-key",
+    default="~/.config/aocd/token",
+    help="Location of AoC session key."
+)
 parser.add_argument("output", help="Path to output file.")
 
 args = parser.parse_args()
 
-with open(args.output, "w") as f:
-    f.write(get_data(day=args.day, year=args.year))
+
+# Check path
+args.output = os.path.abspath(os.path.expanduser(args.output))
+args.session_key = os.path.abspath(os.path.expanduser(args.session_key))
+parent_dir = os.path.dirname(args.output)
+if not os.path.exists(parent_dir):
+    raise Exception("Bad path, parent directory {} does not exist.".format(parent_dir))
+
+if not os.path.exists(args.output):
+    os.makedirs(args.output)
+
+if not os.path.exists(args.session_key):
+    raise Exception("Bad session key path: {}".format(args.session_key))
+
+with open(args.session_key, 'r') as f:
+    SESSION_KEY = f.read().strip()
+
+# Write data file.
+with open(os.path.join(args.output, 'input.txt'), "w") as f:
+    f.write(get_content(args.year, args.day, "input", session_key=SESSION_KEY))
+
+
+# Write description.
+with open(os.path.join(args.output, 'README.md'), 'w') as f:
+    f.write(get_content(args.year, args.day, session_key=SESSION_KEY))
