4d2daaaa73df1ccf8b89ad7ea29dd129663dd8c3
Bendik Samseth <b.samseth@gmail.com>
2018-12-06 21:24:25 +0100

    Change solution to day 6 part 1.
    Not noticeably faster, but _should_ be smarter.

--- aoc-2018/day-06/day_06.py
+++ aoc-2018/day-06/day_06.py
@@ -1,18 +1,41 @@
 import numpy as np
+from itertools import count
 
 points = np.loadtxt('input.txt', dtype=int, delimiter=', ')
 
-# Part 1:
-# Areas that do not change if we increase the grid boundaries
-# are the areas that are finite. Expand the grid until such
-# bounded areas are found, then pick the maximum.
-def areas(x_min, x_max, y_min, y_max, points):
-    areas = np.zeros(len(points), dtype=np.uint32)
-    X = np.arange(x_min, x_max)
-    Y = np.arange(y_min, y_max)
-    X, Y = np.meshgrid(X, Y)
-    for x, y in zip(X.ravel(), Y.ravel()):
-        # Calculate all distances.
+def manhattan_spiral(x0=0, y0=0):
+    """
+    Generate lists of points where the Manhattan distance
+    to (x0, y0) increases by one per list.
+
+    For (x0, y0) = (0, 0):
+
+    [(0, 0)]
+    [(-1, 0), (0, 1), (0, -1), (1, 0)]
+    [(-2, 0), (-1, 1), (-1, -1), (0, 2), (0, -2), (1, 1), (1, -1), (2, 0)]
+    ...
+    """
+    for dist in count(0):
+        points = []
+        for i in range(-dist, dist + 1):
+            points.append((x0 + i, y0 + dist - abs(i)))
+            if dist != abs(i):
+                points.append((x0 + i, y0 + abs(i) - dist))
+        yield points
+
+# Start at the center of the points (not so important where, but a reasonable start).
+x0, y0 = [int(a) for a in np.mean(points, axis=0)]
+
+# Need to at least include all the points in the spiral.
+minimum_spiral_size = np.max(np.abs(x0 - points[:, 0]) + np.abs(y0 - points[:, 1]))
+
+# Walk around the spiral. If one of the areas doesn't change (and we have gone
+# far enough), then this point will not increase in the future either.
+# Return the maximum area for such a finite point as soon as one is found.
+areas_prev = np.zeros(len(points), dtype=int)
+for i, coords in enumerate(manhattan_spiral(x0, y0)):
+    areas = np.zeros_like(areas_prev)
+    for x, y in coords:
         dists = np.abs(x - points[:, 0]) + np.abs(y - points[:, 1])
         closest_first = np.argmin(dists)
         closest_last = np.argmin(dists[::-1])
@@ -20,24 +43,11 @@ def areas(x_min, x_max, y_min, y_max, points):
         if closest_first == len(points) - closest_last - 1:  # No tie, someone is closest
             areas[closest_first] += 1
 
-    return areas
+    if i > minimum_spiral_size:
+        bounded = areas < 1
+        if np.any(areas_prev[bounded] > 0):
+            print("Largest finite areas:", np.max((areas_prev + areas)[bounded]))
+            break
 
-
-x_min, y_min = np.min(points, axis=0) - 5
-x_max, y_max = np.max(points, axis=0) + 5
-
-areas_prev = areas(x_min, x_max, y_min, y_max, points)
-while True:
-    x_min -= 5
-    x_max += 5
-    y_min -= 5
-    y_max += 5
-
-    areas_new = areas(x_min, x_max, y_min, y_max, points)
-
-    bounded = areas_new - areas_prev == 0
-    if np.any(bounded):
-        print(np.max(areas_new[bounded]))
-        break
-
+    areas_prev += areas
 
