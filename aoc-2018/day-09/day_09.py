44297c68e2516da8258f84fcecf8bc82e784a5eb
Bendik Samseth <b.samseth@gmail.com>
2018-12-09 23:48:11 +0100

    Solve day 9.
    Part two takes ~30 secs. There's likely some direct solution that
    does not need to simulate the play...

---
+++ aoc-2018/day-09/day_09.py
@@ -0,0 +1,61 @@
+from itertools import cycle
+import re
+
+
+class Marble(object):
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left if left else self
+        self.right = right if right else self
+
+    def place_between(self, left, right):
+        self.left = left
+        self.right = right
+        left.right = self
+        right.left = self
+        return self
+
+    def remove(self):
+        self.left.right = self.right
+        self.right.left = self.left
+
+
+class Circle(object):
+    def __init__(self):
+        self.current = Marble(0)
+
+    def place_marble(self, value):
+        if value % 23 != 0:
+            self.current = Marble(value).place_between(self.current.right, self.current.right.right)
+            return 0
+
+        marble = self.current
+        for _ in range(7):
+            marble = marble.left
+        self.current = marble.right
+        marble.remove()
+        return value + marble.value
+
+    def __str__(self):
+        s = [self.current.value]
+        marble = self.current.right
+        while marble != self.current:
+            s.append(marble.value)
+            marble = marble.right
+        start = s.index(0)
+        return ' '.join(str(m) for m in s[start:] + s[:start]).replace(f'{self.current.value}', f'({self.current.value})')
+
+def play(n_players, largest_marble):
+    players = {p: 0 for p in range(1, n_players+1)}
+    circle = Circle()
+    for marble, player in zip(range(1, largest_marble+1), cycle(players)):
+        players[player] += circle.place_marble(marble)
+        # print(f'[{player}]', circle)
+    return max(players.values())
+
+
+with open('input.txt', 'r') as f:
+    n_players, largest_marble = [int(_) for _ in re.match('([0-9]+)[^0-9]*([0-9]+)', f.read()).groups()]
+
+print('Winning elf\' score:', play(n_players, largest_marble))
+print('Winning elf\' score with 100 times more marbles:', play(n_players, largest_marble * 100))
