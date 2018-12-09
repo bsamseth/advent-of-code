bc5d61b4570d612f3cfc06a80ba051b016fd1798
Bendik Samseth <b.samseth@gmail.com>
2018-12-10 00:12:56 +0100

    Refactor day 9.
    The shorter version, using a deque rather than implementing the linked
    list our selfs. Shorter and faster (deque is built-in with C backend).
    Same essence though. Inspired from reading other solutions.

--- aoc-2018/day-09/day_09.py
+++ aoc-2018/day-09/day_09.py
@@ -1,49 +1,21 @@
 from itertools import cycle
+from collections import defaultdict, deque
 import re
 
 
-class Marble(object):
-    def __init__(self, value, left=None, right=None):
-        self.value = value
-        self.left = left if left else self
-        self.right = right if right else self
-
-    def place_between(self, left, right):
-        self.left = left
-        self.right = right
-        left.right = self
-        right.left = self
-        return self
-
-    def remove(self):
-        self.left.right = self.right
-        self.right.left = self.left
-
-
-class Circle(object):
-    def __init__(self):
-        self.current = Marble(0)
-
-    def place_marble(self, value):
-        if value % 23 != 0:
-            self.current = Marble(value).place_between(
-                self.current.right, self.current.right.right
-            )
-            return 0
-
-        marble = self.current
-        for _ in range(7):
-            marble = marble.left
-        self.current = marble.right
-        marble.remove()
-        return value + marble.value
-
-
 def play(n_players, largest_marble):
-    players = {p: 0 for p in range(1, n_players + 1)}
-    circle = Circle()
-    for marble, player in zip(range(1, largest_marble + 1), cycle(players)):
-        players[player] += circle.place_marble(marble)
+    players = defaultdict(int)
+    circle = deque([0])
+
+    # The "current" is at the end of the deque at all times.
+    for marble, player in zip(range(1, largest_marble + 1), cycle(range(n_players))):
+        if marble % 23 == 0:
+            circle.rotate(7)
+            players[player] += marble + circle.pop()
+            circle.rotate(-1)
+        else:
+            circle.rotate(-1)
+            circle.append(marble)
     return max(players.values())
 
 
