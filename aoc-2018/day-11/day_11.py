500722ffff200917bc7cc2cb93797022f9ec6863
Bendik Samseth <b.samseth@gmail.com>
2018-12-12 17:01:00 +0100

    Refactor day 11 (more than 100x speedup).
    Precomputes the partial sums of the grid so that computing the
    square sums is really efficient. This brings it down to about ~15
    seconds. Using numba as well this is taken down to ~1 sec!

--- aoc-2018/day-11/day_11.py
+++ aoc-2018/day-11/day_11.py
@@ -1,38 +1,59 @@
 import numpy as np
+import numba
 
 def fuel_level(x, y, serial_number=0):
+    """Calculate fuel level for the given coordinate(s)."""
     rack_id = x + 10
     return (((rack_id * y + serial_number) * rack_id) // 100) % 10 - 5
 
-def largest_nxn(grid, n):
-    size = len(grid)
-    max_corner = None, None
-    max_fuel = -float('inf')
-    for i in range(size - n - 1):
-        for j in range(size - n - 1):
-            fuel = np.sum(grid[i:i+n, j:j+n])
-            if fuel > max_fuel:
-                max_fuel = fuel
-                max_corner = j+1, i+1
-    return max_fuel, max_corner
+@numba.njit
+def compute_partial_sum(grid):
+    partial = np.copy(grid)
+    for i in range(partial.shape[0]):
+        for j in range(partial.shape[1]):
+            if i > 0:
+                partial[i, j] += partial[i-1, j]
+            if j > 0:
+                partial[i, j] += partial[i, j-1]
+            if i > 0 and j > 0:
+                partial[i, j] -= partial[i-1, j-1]
+    return partial
 
-serial_number = np.loadtxt('input.txt', dtype=int)
-x = np.arange(1, 301, dtype=int)
-x, y = np.meshgrid(x, x)
-grid = fuel_level(x, y, serial_number=serial_number)
+# No njit due to creation and returning of arrays. But
+# jit handles this so that it can njit the loops still, and
+# just jiting the parts around. Major (!) speed boost.
+@numba.autojit
+def find_optimal_corners(grid):
+    partial = compute_partial_sum(grid)
+    grid_size = len(grid)
+    maxes = - np.ones(grid_size+1, dtype=int) * 5 * grid_size * (grid_size+1)
+    corners = np.empty((grid_size+1, 2), dtype=int)
+    for size in range(1, grid_size+1):
+        for i in range(partial.shape[0] - size):
+            for j in range(partial.shape[1] - size):
+                square_sum = partial[i+size-1, j+size-1]
+                if i > 0:
+                    square_sum -= partial[i-1, j+size-1]
+                if j > 0:
+                    square_sum -= partial[i+size-1, j-1]
+                if i > 0 and j > 0:
+                    square_sum += partial[i-1, j-1]
+                if square_sum > maxes[size]:
+                    maxes[size] = square_sum
+                    corners[size] = j+1, i+1
+    return maxes, corners
 
-# Part 1
-largest_total_power, corner = largest_nxn(grid, 3)
-print('Top left corner of max 3x3 grid:', corner)
+if __name__ == "__main__":
 
+    serial_number = np.loadtxt('input.txt', dtype=int)
+    x = np.arange(1, 301, dtype=int)
+    x, y = np.meshgrid(x, x)
 
-# Part 2
-max_fuel = -float('inf')
-for n in range(1, 301):
-    fuel, corner = largest_nxn(grid, n)
-    if fuel > max_fuel:
-        max_fuel = fuel
-        max_corner = corner
-        n_max = n
+    # This is the 300 x 300 fuel grid.
+    grid = fuel_level(x, y, serial_number=serial_number)
 
-print(f'Top left corner of max {n_max}x{n_max} grid:', max_corner)
+    maxes, corners = find_optimal_corners(grid)
+    print('Max corner for 3x3 grid:', corners[3])
+    max_size = np.argmax(maxes)
+    print('Max corner for optimal grid:', corners[max_size], max_size)
+
