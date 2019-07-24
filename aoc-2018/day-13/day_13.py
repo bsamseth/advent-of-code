93b2c31c9da05d4438b1919fceaaac1ca42be4a7
Bendik Samseth <b.samseth@gmail.com>
2019-07-24 15:21:52 +0200

    Import solution to day 13
    Not going to try to debug that one more time. Much nicer solution based
    on complex numbers, found on reddit.

--- aoc-2018/day-13/day_13.py
+++ aoc-2018/day-13/day_13.py
@@ -1,87 +1,36 @@
 from itertools import cycle
-import re
-from colorama import Fore, Style
 
-class Cart(object):
-    carts_chars = ["<", ">", "^", "v"]
-    direction = dict(zip(carts_chars, [(-1, 0), (1, 0), (0, -1), (0, 1)]))
-    left = dict(zip(carts_chars, "v^<>"))
-    straight = dict(zip(carts_chars, carts_chars))
-    right = dict(zip(carts_chars, "^v><"))
-    forward_slash = dict(zip(carts_chars, "v^><"))
-    backward_slash = dict(zip(carts_chars, "^v<>"))
-    def __init__(self, symbol, x, y):
-        self.symbol, self.x, self.y = symbol, x, y
-        self.turns = cycle([Cart.left, Cart.straight, Cart.right])
-        self.crashed = False
+with open("input.txt", "r") as f:
+    data = f.read()
 
-    def move(self, grid):
-        dx, dy = Cart.direction[self.symbol]
-        self.x, self.y = self.x + dx, self.y + dy
+m = {}; carts = {}; dir_d = {'<': -1, '>': 1, '^': 1j, 'v': -1j}
+for i, row in enumerate(data.split("\n")):
+    for j, c in enumerate(row):
+        loc = j-i*1j
+        if c in r'/\+':
+            m[loc] = c
+        elif c in dir_d:
+            carts[loc] = dir_d[c], cycle([1j, 1, -1j])
 
-        if grid[self.y][self.x] in Cart.carts_chars:
-            print("Collision!", self.x, self.y)
-            return False,  # Crash!
+while len(carts) > 1:
+    for loc in sorted(carts, key=lambda x: (-x.imag, x.real)):
+        if loc not in carts:
+            continue  # deleted due to collision
+        dxn, turn = carts.pop(loc)  # take out cart
+        loc += dxn  # update position
 
-        if grid[self.y][self.x] == "+":
-            self.symbol = next(self.turns)[self.symbol]
-        elif grid[self.y][self.x] == "/":
-            self.symbol = Cart.forward_slash[self.symbol]
-        elif grid[self.y][self.x] == "\\":
-            self.symbol = Cart.backward_slash[self.symbol]
+        if loc in carts:  # handle collision
+            print('collision!', f"{int(loc.real)},{int(-loc.imag)}")
+            del carts[loc]
+            continue
 
-        return True, self.x, self.y, self.symbol
+        track = m.get(loc)  # update direction
+        if track == '+':
+            dxn = dxn * next(turn)
+        elif track is not None: #/ or \
+            dxn *= 1j * (2*((track == '/') ^ (dxn.real == 0))-1)
 
-    def __repr__(self):
-        return f"Cart({self.symbol}, {self.x}, {self.y})"
+        carts[loc] = dxn, turn  # put cart back onto tracks
 
-
-class Grid(object):
-    def __init__(self, filename):
-        with open(filename, "r") as f:
-            self.grid = [[char for char in line if char != "\n"] for line in f if line != "\n"]
-
-        # Input should be formated with equal width per row, but editors might muck this up.
-        assert all(len(row) == len(self.grid[0]) for row in self.grid)
-
-        # Obtain a copy of the grid with all carts removed. Needed to restore
-        # grid positions after carts move.
-        self.empty_grid = [
-            list(
-                "".join(row)
-                .replace(">", "-")
-                .replace("<", "-")
-                .replace("^", "|")
-                .replace("v", "|")
-            )
-            for row in self.grid
-        ]
-        self.carts = [Cart(char, j, i) for i, row in enumerate(self.grid) for j, char in enumerate(row) if char in Cart.carts_chars]
-
-    def __str__(self):
-        s = "\n".join("".join(row) for row in self.grid)
-        s = re.sub("[<>^v]", lambda match: f"{Fore.RED}{Style.BRIGHT}{match.group(0)}{Style.RESET_ALL}", s)
-        return s
-
-
-    def tick(self):
-        for cart in filter(lambda c: not c.crashed, self.carts):
-            self.grid[cart.y][cart.x] = self.empty_grid[cart.y][cart.x]
-            success, *result = cart.move(self.grid)
-            if not success:
-                self.grid[cart.y][cart.x] = self.empty_grid[cart.y][cart.x]
-                for c in self.carts:
-                    if cart.x == c.x and cart.y == c.y:
-                        c.crashed = True
-            else:
-                x, y, symbol = result
-                self.grid[y][x] = symbol
-        self.carts = list(filter(lambda c: not c.crashed, self.carts))
-
-if __name__ == "__main__":
-    grid = Grid('input.txt')
-    while len(grid.carts) > 1:
-        # print(grid)
-        grid.tick()
-    print(grid)
-    print(grid.carts)
+pos, *_ = list(carts)
+print(f"Final cart position: {int(pos.real)},{-int(pos.imag)}")
