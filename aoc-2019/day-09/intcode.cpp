f909792c2e8252051f9fe5942f5356f704ec5910
Bendik Samseth <b.samseth@gmail.com>
2019-12-14 10:24:21 +0100

    Init day 9 with intcode from day 7

---
+++ aoc-2019/day-09/intcode.cpp
@@ -0,0 +1,79 @@
+#include "intcode.hpp"
+
+#include <utility>
+
+std::vector<int> read_program(const std::string& filename)
+{
+    std::ifstream in_file {filename};
+    std::vector<int> program;
+    while (in_file.good())
+    {
+        char comma;
+        int token;
+        in_file >> token;
+        in_file >> comma;
+        assert(comma == ',' || in_file.eof());
+        program.push_back(token);
+    }
+    return program;
+}
+
+/* Load parameter from program memory, using the appropriate mode. */
+static inline int
+    read_parameter(const std::vector<int>& program, int ip, bool immediate = true)
+{
+    return immediate ? program[ip] : program[program[ip]];
+}
+
+bool Process::execute_inst(const Opcode& opcode, int& ip)
+{
+    int a, b, c;
+    switch (opcode.op)
+    {
+        // Handle single parameter instructions (I/O).
+        case Inst::INP: program[program[ip++]] = inputs->pop(); return true;
+        case Inst::OUT:
+            outputs->push(read_parameter(program, ip++, opcode.a_immediate));
+            return true;
+
+        // Handle two-parameter instructions (conditional jumps).
+        case Inst::JT:
+        case Inst::JNT:
+            a = read_parameter(program, ip++, opcode.a_immediate);
+            b = read_parameter(program, ip++, opcode.b_immediate);
+            if ((a && opcode.op == Inst::JT) || (!a && opcode.op == Inst::JNT))
+                ip = b;
+            return true;
+
+        // Handle three-parameter instructions (e.g. c = f(a, b))
+        case Inst::ADD:
+        case Inst::MUL:
+        case Inst::LT:
+        case Inst::EQ:
+            a = read_parameter(program, ip++, opcode.a_immediate);
+            b = read_parameter(program, ip++, opcode.b_immediate);
+            c = read_parameter(program, ip++);
+            program[c] = opcode.op == Inst::ADD
+                             ? a + b
+                             : opcode.op == Inst::MUL
+                                   ? a * b
+                                   : ((a < b && opcode.op == Inst::LT)
+                                      || (a == b && opcode.op == Inst::EQ))
+                                         ? 1
+                                         : 0;
+            return true;
+        default: return false;  // Halt.
+    }
+}
+
+Process::Process(std::vector<int> prog,
+                 std::shared_ptr<IOQueue<int>> in,
+                 std::shared_ptr<IOQueue<int>> out)
+    : program(std::move(prog)), inputs(std::move(in)), outputs(std::move(out))
+{
+    executor = std::thread {[=]() {
+        int ip = 0;
+        while (execute_inst(Opcode {program[ip++]}, ip))
+            ;
+    }};
+}
---
+++ aoc-2019/day-09/intcode.hpp
@@ -0,0 +1,89 @@
+#pragma once
+
+#include <cassert>
+#include <condition_variable>
+#include <deque>
+#include <fstream>
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <vector>
+
+/* Supported Intcode instructions. */
+enum class Inst {
+    ADD = 1,
+    MUL,
+    INP,
+    OUT,
+    JT,
+    JNT,
+    LT,
+    EQ,
+    HLT = 99
+};
+
+/* An Opcode stores the operation to execute and the associated parameter modes. */
+struct Opcode {
+    Inst op;
+    bool a_immediate;
+    bool b_immediate;
+
+    explicit Opcode(int x)
+            : op(Inst{x % 100}), a_immediate((x % 1000) / 100), b_immediate((x % 10000) / 1000) {
+    }
+};
+
+/* Read a program from file, return as a vector of intcodes. */
+std::vector<int> read_program(const std::string &filename);
+
+template<typename Data>
+class IOQueue {
+private:
+    std::mutex lock;
+    std::condition_variable waiting;
+    std::deque<Data> data;
+
+public:
+    Data pop() {
+        std::unique_lock<std::mutex> guard(lock);
+        waiting.wait(guard, [=] { return data.size() > 0; });
+        Data value = data.front();
+        data.pop_front();
+        return value;
+    }
+
+    void push(Data d) {
+        std::lock_guard<std::mutex> guard(lock);
+        data.push_back(d);
+        waiting.notify_one();
+    }
+
+    [[nodiscard]] auto size() const noexcept { return data.size(); }
+
+    [[nodiscard]] const auto &get_data() const noexcept { return data; }
+};
+
+class Process {
+private:
+    std::vector<int> program;
+    std::shared_ptr<IOQueue<int>> inputs;
+    std::shared_ptr<IOQueue<int>> outputs;
+    std::thread executor;
+
+    bool execute_inst(const Opcode &opcode, int &ip);
+
+public:
+    explicit Process(const std::string &filename) : Process(read_program(filename)) {}
+
+    explicit Process(const std::vector<int> &program)
+            : Process(program,
+                      std::make_shared<IOQueue<int>>(),
+                      std::make_shared<IOQueue<int>>()) {
+    }
+
+    Process(std::vector<int> program,
+            std::shared_ptr<IOQueue<int>> inputs,
+            std::shared_ptr<IOQueue<int>> outputs);
+
+    void join() { executor.join(); }
+};
