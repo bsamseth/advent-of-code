9d9509cb47e8810cd2da4ff6b0c4e063dbbe98b5
Bendik Samseth <b.samseth@gmail.com>
2019-12-22 00:59:19 +0100

    Solve day 14 part 2
    Part two much better!

--- aoc-2019/day-14/day_14.py
+++ aoc-2019/day-14/day_14.py
@@ -1,7 +1,7 @@
-from dataclasses import dataclass
 from collections import defaultdict
-from typing import Dict, List
+from dataclasses import dataclass
 from math import ceil
+from typing import Dict, List
 
 
 @dataclass
@@ -48,6 +48,26 @@ def ore_required_for(target: Reactant, reactions: Dict[str, Reaction]) -> int:
     return ore
 
 
+def binary_search(max_ore: int, lower: int, upper: int, reactions: Dict[str, Reaction]):
+    """Return the max fuel possible to produce with the given ore supply."""
+
+    assert lower < upper
+
+    while lower + 1 < upper:
+        mid = (lower + upper) // 2
+        value = ore_required_for(Reactant(ID="FUEL", quantity=mid), reactions)
+
+        if value > max_ore:
+            upper = mid
+        elif value < max_ore:
+            lower = mid
+        else:
+            lower = upper = mid
+
+    return lower
+
+
+# Parse input:
 reactions: Dict[str, Reaction] = {}
 with open("input.txt") as f:
     for line in f:
@@ -61,5 +81,17 @@ with open("input.txt") as f:
 
         reactions[result.ID] = Reaction(result, reactants)
 
+ore_to_fuel = ore_required_for(Reactant(ID="FUEL", quantity=1), reactions)
+print("Part 1:", ore_to_fuel)
 
-print("Part 1", ore_required_for(Reactant(ID="FUEL", quantity=1), reactions))
+# For the binary search, ore / ore_per_fuel is a lower limit, and twice that is a solid upper limit.
+ore_available = 1000000000000
+print(
+    "Part 2:",
+    binary_search(
+        max_ore=ore_available,
+        lower=ore_available // ore_to_fuel,
+        upper=ore_available // ore_to_fuel * 2,
+        reactions=reactions,
+    ),
+)
