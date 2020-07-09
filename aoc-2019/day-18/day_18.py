b1761386ff99538f0fec5cc6a5b7892064527875
Bendik Samseth <b.samseth@gmail.com>
2020-07-09 09:50:53 +0200

    Commit start on day 18

--- aoc-2019/day-18/day_18.py
+++ aoc-2019/day-18/day_18.py
@@ -3,6 +3,7 @@ from functools import lru_cache
 from time import time
 
 import networkx as nx
+from networkx.exception import NetworkXNoPath
 
 
 def add_key_to(keys, key):
@@ -26,89 +27,110 @@ class Grid:
             self.grid = [list(line.strip()) for line in f]
 
         dot_count = 0
+        robot_count = 0
         self.graph = nx.Graph()
         for i, row in enumerate(self.grid):
             for j, c in enumerate(row):
                 if c == "#":
                     continue
-
-                if c == "@":
-                    self.pos = i, j, "@"
-
-                if c == "." and (
-                    (
-                        row[j - 1] != "#"
-                        and row[j + 1] != "#"
-                        and self.grid[i - 1][j] == "#"
-                        and self.grid[i + 1][j] == "#"
-                    )
-                    or (
-                        row[j - 1] == "#"
-                        and row[j + 1] == "#"
-                        and self.grid[i - 1][j] != "#"
-                        and self.grid[i + 1][j] != "#"
-                    )
-                ):
-                    # Node is not interesting, just a hallway.
-                    continue
-
-                if c == ".":
+                elif c == ".":
                     c = f".{dot_count}"
                     self.grid[i][j] = c
                     dot_count += 1
+                elif c == "@":
+                    c = f"@{robot_count}"
+                    self.grid[i][j] = c
+                    robot_count += 1
 
-                self.graph.add_node(c)
+                if self.grid[i - 1][j] != "#":
+                    self.graph.add_edge(c, self.grid[i - 1][j], weight=1)
+                if self.grid[i][j - 1] != "#":
+                    self.graph.add_edge(c, self.grid[i][j - 1], weight=1)
 
-                for k in reversed(range(j)):
-                    if self.grid[i][k] in self.graph:
-                        self.graph.add_edge(self.grid[i][k], c, weight=j - k)
-                        break
-                    elif self.grid[i][k] == "#":
-                        break
-                for k in reversed(range(i)):
-                    if self.grid[k][j] in self.graph:
-                        self.graph.add_edge(self.grid[k][j], c, weight=i - k)
-                        break
-                    elif self.grid[k][j] == "#":
-                        break
-
+        self.robots = tuple(f"@{i}" for i in range(robot_count))
         self.all_keys = [c for row in self.grid for c in row if c.islower()]
         self.all_keys_set = keys_from_list(self.all_keys)
+
+        # Prune graph: Remove all empty nodes ('.') that can be shortcutted.
+        updated = True
+        while updated:
+            updated = False
+            for a in self.graph.nodes():
+                if "." in a:
+                    neighbors = list(self.graph.neighbors(a))
+                    if len(neighbors) == 1:
+                        self.graph.remove_node(a)
+                        updated = True
+                        break
+                    if len(neighbors) == 2:
+                        neighbor, other_neighbor = neighbors
+                        self.graph.add_edge(
+                            neighbor,
+                            other_neighbor,
+                            weight=self.graph[neighbor][a]["weight"]
+                            + self.graph[a][other_neighbor]["weight"],
+                        )
+                        self.graph.remove_node(a)
+                        updated = True
+                        break
 
     @lru_cache(maxsize=None)
     def path_from_to(self, f, t):
         if f > t:
             return self.path_from_to(t, f)
 
-        path = nx.shortest_path(self.graph, f, t, weight="weight")
+        try:
+            path = nx.shortest_path(self.graph, f, t, weight="weight")
+        except NetworkXNoPath:
+            return False, None, None
         return (
+            True,
             sum(
                 self.graph[path[i]][path[i + 1]]["weight"] for i in range(len(path) - 1)
             ),
             keys_from_list([node.lower() for node in path if node.isupper()]),
         )
 
-    def solve_my(self):
-        distances = {("@", 0): 0}
-        queue = [(0, "@", 0)]
+    def solve(self):
+        distances = {(self.robots, 0): 0}
+        queue = [(0, self.robots, 0)]
 
         while queue:
-            dist, cur_key, cur_keys = heapq.heappop(queue)
+            dist, robots, cur_keys = heapq.heappop(queue)
 
             if cur_keys == self.all_keys_set:
                 return dist
 
-            for new_key in self.all_keys:
-                if not keys_contain(cur_keys, new_key):
-                    length, doors_blocking = self.path_from_to(cur_key, new_key)
-                    if cur_keys & doors_blocking == doors_blocking:
-                        new_dist = dist + length
-                        new_keys = add_key_to(cur_keys, new_key)
-                        if distances.get((new_key, new_keys), float("inf")) > new_dist:
-                            distances[(new_key, new_keys)] = new_dist
-                            heapq.heappush(queue, (new_dist, new_key, new_keys))
+            for i, robot in enumerate(robots):
+                for new_key in self.all_keys:
+                    if not keys_contain(cur_keys, new_key):
+                        path_exists, length, doors_blocking = self.path_from_to(
+                            robot, new_key
+                        )
+                        if path_exists and (
+                            cur_keys & doors_blocking == doors_blocking
+                        ):
+                            new_dist = dist + length
+                            new_keys = add_key_to(cur_keys, new_key)
+                            new_robots = robots[:i] + (new_key,) + robots[i + 1 :]
+                            if (
+                                distances.get((new_robots, new_keys), float("inf"))
+                                > new_dist
+                            ):
+                                distances[(new_robots, new_keys)] = new_dist
+                                heapq.heappush(
+                                    queue, (new_dist, new_robots, new_keys,),
+                                )
 
 
 t0 = time()
 grid = Grid("input.txt")
-print(f"Part 1: {grid.solve_my()} (solved in {round(time() - t0, 2)} seconds)")
+print(f"Part 1: {grid.solve()} (solved in {round(time() - t0, 2)} seconds)")
+
+t0 = time()
+grid = Grid("input-part2.txt")
+print(f"Part 2: {grid.solve()} (solved in {round(time() - t0, 2)} seconds)")
+# nx.draw_planar(grid.graph, with_labels=True)
+# import matplotlib.pyplot as plt
+#
+# plt.show()
