014830036735cd7aff97a9d06322ba273c503ce3
Bendik Samseth <b.samseth@gmail.com>
2020-12-15 08:01:12 +0100

    Solve day 15

---
+++ aoc-2020/day-15/day_15.py
@@ -0,0 +1,32 @@
+import time
+from numba import njit
+from numba.typed import Dict
+
+
+@njit
+def play_till_round(max_round, memory, last_number, n_starting_numbers):
+    for round_nr in range(n_starting_numbers + 1, max_round + 1):
+        next_number = round_nr - 1 - memory.get(last_number, round_nr - 1)
+        memory[last_number] = round_nr - 1
+        last_number = next_number
+    return next_number
+
+
+def initial_memory(numbers):
+    memory = Dict()  # Numba dict must be initialized like this:
+    for i, n in enumerate(numbers[:-1]):
+        memory[n] = i + 1
+    return memory
+
+
+with open("input.txt") as f:
+    starting_numbers = list(map(int, f.read().strip().split(",")))
+
+t0 = time.time()
+last_number, round_nr = starting_numbers[-1], len(starting_numbers)
+for part, n in ((1, 2020), (2, 30_000_000)):
+    print(
+        f"Part {part}:",
+        play_till_round(n, initial_memory(starting_numbers), last_number, round_nr),
+    )
+print(f"Total time: {round(time.time() - t0, 2)} seconds")
