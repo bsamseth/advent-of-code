72a4fbcb10772963d1d19405b5a7d42fa4e1cd2d
Bendik Samseth <b.samseth@gmail.com>
2020-12-29 11:21:58 +0100

    Solve day 23 better
    Linked list was intuitive. This implementation is the same idea, but the entire data structure is encoded in a single array, which makes this about 3x faster.

--- aoc-2020/day-23/day_23.py
+++ aoc-2020/day-23/day_23.py
@@ -1,82 +1,56 @@
 import time
 
+from toolz import sliding_window
+
 t0 = time.time()
 
 
-class Node:
-    def __init__(self, value, next=None):
-        self.value, self.next = value, next
+def move(curr, successors):
+    a = successors[curr]
+    b = successors[a]
+    c = successors[b]
+    after_c = successors[c]
 
-    def __repr__(self):
-        return f"Node({self.value})"
-
-
-class CircularList:
-    def __init__(self, iterable=()):
-        self.head = self.tail = None
-        self.value_to_node = {v: self._append(v) for v in iterable}
-
-    def __len__(self):
-        return len(self.value_to_node)
-
-    def __iter__(self):
-        if self.head is None:
-            return iter(())
-        curr = self.head
-        while True:
-            yield curr.value
-            curr = curr.next
-            if curr == self.head:
-                break
-
-    def __getitem__(self, v):
-        return self.value_to_node[v]
-
-    def _append(self, v):
-        n = Node(v, next=self.head)
-        if self.tail is None:
-            self.head = self.tail = n
-            n.next = n
-        else:
-            self.tail.next = n
-            self.tail = n
-        return n
-
-    def rotate(self):
-        self.head, self.tail = self.head.next, self.head
-
-
-def move(order: CircularList) -> None:
-    curr = order.head
-    a = order.head.next
-    b = order.head.next.next
-    c = order.head.next.next.next
-
-    d = curr.value
-    while (d := d - 1) in (a.value, b.value, c.value) or d == 0:
+    d = curr
+    while (d := d - 1) in (a, b, c) or d == 0:
         if d == 0:
-            d = len(order) + 1
+            d = len(successors)
 
-    curr.next = c.next
-    d = order[d]
-    c.next = d.next
-    d.next = a
-    order.rotate()
+    successors[curr] = after_c
+    successors[c] = successors[d]
+    successors[d] = a
+    return after_c
+
+
+def solve(order, moves):
+    # Build successor array:
+    successors = [0] * (len(order) + 1)
+    for a, b in sliding_window(2, order):
+        successors[a] = b
+    successors[order[-1]] = order[0]
+
+    # Perform moves:
+    curr = order[0]
+    for _ in range(moves):
+        curr = move(curr, successors)
+    return successors
+
+
+def iterate_successors(successors):
+    """Generate all elements in the order given by successors, starting at 1."""
+    curr = 1
+    yield 1
+    while (curr := successors[curr]) != 1:
+        yield curr
 
 
 with open("input.txt") as f:
     starting_order = list(map(int, list(f.read().strip())))
 
-ll = CircularList(starting_order)
-for _ in range(100):
-    move(ll)
-while ll.head.value != 1:
-    ll.rotate()
-print("Part 1:", "".join(map(str, ll))[1:])
+print("Part 1:", "".join(map(str, iterate_successors(solve(starting_order, 100))))[1:])
 
-
-ll = CircularList(starting_order + list(range(len(starting_order) + 1, 1_000_000 + 1)))
-for _ in range(10_000_000):
-    move(ll)
-print("Part 2:", ll[1].next.value * ll[1].next.next.value)
-print(f"Total time: {round(time.time()-t0, 2)} seconds")
+successors = solve(
+    starting_order + list(range(len(starting_order) + 1, 1_000_000 + 1)), 10_000_000
+)
+print("Part 2:", successors[1] * successors[successors[1]])
+print(f"Total time: {round(time.time() - t0, 2)} seconds")
