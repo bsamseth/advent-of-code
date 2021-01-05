c7460b4a1aee68ed55a0b0448dbd1cf8b6098728
Bendik Samseth <b.samseth@gmail.com>
2021-01-05 07:30:56 +0100

    Solve day 10 2015

---
+++ aoc-2015/day-10/day_10.hs
@@ -0,0 +1,18 @@
+-- stack --resolver lts-16.27 script --optimize
+
+main :: IO ()
+main = do
+  content <- readFile "input.txt"
+  putStrLn $ "Part 1: " ++ (show . length) ((iterate lookAndSay content) !! 40)
+  putStrLn $ "Part 2: " ++ (show . length) ((iterate lookAndSay content) !! 50)
+
+
+lookAndSay :: String -> String
+lookAndSay (s:rest) = ls 1 s rest
+  where
+    ls :: Int -> Char -> String -> String
+    ls n c (x:xs)
+      | c == x = ls (n+1) c xs
+      | otherwise = (show n) ++ (c:[]) ++ (ls 1 x xs)
+    ls n c [] = (show n) ++ (c:[])
+lookAndSay [] = error ("Not a valid string.")
