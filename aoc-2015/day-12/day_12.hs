9336b8a500c4df630ef526b25ebbde874dc568a0
Bendik Samseth <b.samseth@gmail.com>
2021-01-07 23:39:35 +0100

    Solve day 12 2015

---
+++ aoc-2015/day-12/day_12.hs
@@ -0,0 +1,22 @@
+-- stack --resolver lts-16.27 script --optimize --package aeson,vector,unordered-containers
+
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.Aeson 
+import Data.Maybe (fromJust)
+import Data.Vector (toList)
+import qualified Data.HashMap.Strict as M
+
+main :: IO ()
+main = do
+  jsonMaybe <- decodeFileStrict "input.txt" :: IO (Maybe Value)
+  let jsonData = fromJust jsonMaybe
+  putStrLn $ "Part 1: " ++ (show $ (round $ addJson False jsonData :: Int))
+  putStrLn $ "Part 2: " ++ (show $ (round $ addJson True jsonData :: Int))
+    where
+      addJson _ (Number x) = x
+      addJson red (Array a) = sum $ map (addJson red) $ toList a
+      addJson red (Object o)
+        | red && String "red" `elem` M.elems o = 0
+        | otherwise = sum $ map (addJson red) $ M.elems o
+      addJson _ _ = 0
