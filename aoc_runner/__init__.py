acfca1df8fb6d2af39026e4251cee3fdc157cc75
Bendik Samseth <b.samseth@gmail.com>
2021-12-26 22:20:56 +0100

    imporve plugin for aoc

--- aoc_runner/__init__.py
+++ aoc_runner/__init__.py
@@ -1,27 +1,37 @@
-import io
+import re
 import subprocess
-import sys
 from pathlib import Path
 
 
-def plugin(year, day, **kwargs):
-    script = Path(__file__).parent.parent / f"aoc-{year}/day-{day:02d}/day_{day}.py"
-
-    if not script.exists():
-        return None, None
-
-    old_stdout = sys.stdout
-    sys.stdout = io.StringIO()
+def _as_int(x):
     try:
-        p = subprocess.run(
-            ["poetry", "run", "python", f"day_{day}.py"],
-            capture_output=True,
-            cwd=script.parent,
-        )
-    finally:
-        sys.stdout = old_stdout
+        return int(x)
+    except ValueError:
+        return x
 
-    part_1, *part_2 = [
-        int(x.strip().split(" ")[-1]) for x in p.stdout.decode().splitlines()
-    ]
-    return part_1, part_2[0] if part_2 else None
+
+def plugin(year, day, **kwargs):
+    script = (
+        Path(__file__).parent.parent / f"aoc-{year}/day-{day:02d}/day_{day}"
+    ).absolute()
+
+    if script.exists():
+        # A binary already exists, so we can just run it and assume it is fast:
+        command = [script]
+    elif script.with_suffix(".cpp").exists():
+        command = (
+            f"g++ -std=c++17 -O3 -o {script} {script.with_suffix('.cpp')} && {script}"
+        )
+    elif script.with_suffix(".hs").exists():
+        command = f"stack {script.with_suffix('.hs')}"
+    elif script.with_suffix(".py").exists():
+        command = f"python {script.with_suffix('.py')}"
+    else:
+        raise FileNotFoundError("No code for this day.")
+
+    p = subprocess.run(command, capture_output=True, cwd=script.parent, shell=True)
+
+    part_1, *part_2 = re.findall(
+        r"^[pP]art.*:\s*(.+)$", p.stdout.decode(), flags=re.MULTILINE
+    )
+    return _as_int(part_1), _as_int(part_2[0]) if part_2 else None
