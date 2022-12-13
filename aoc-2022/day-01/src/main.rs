2a1541944362bfa8b1dd91469de6ac30d3d60d4a
Bendik Samseth <b.samseth@gmail.com>
2022-12-13 16:50:09 +0100

    use macro functions for aocd

--- aoc-2022/day-01/src/main.rs
+++ aoc-2022/day-01/src/main.rs
@@ -1,15 +1,13 @@
-use aocd::Aocd;
+use aocd::*;
 
+#[aocd(2022, 1)]
 fn main() {
-    let client = Aocd::new(2022, 1);
-    let input = client.get_input();
-
-    let mut elves: Vec<_> = input
+    let mut elves: Vec<_> = input!()
         .split("\n\n")
         .map(|e| e.lines().map(|l| l.parse::<u32>().unwrap()).sum())
         .collect();
     elves.sort();
 
-    client.submit(1, elves.last().unwrap());
-    client.submit(2, elves.iter().rev().take(3).sum::<u32>());
+    submit!(1, elves.last().unwrap());
+    submit!(2, elves.iter().rev().take(3).sum::<u32>());
 }
