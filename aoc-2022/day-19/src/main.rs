694b74ca4de9746efcc56dfe9243e6d9376074e5
Bendik Samseth <b.samseth@gmail.com>
2022-12-23 12:47:05 +0100

    solve day 19 2022

--- aoc-2022/day-19/src/main.rs
+++ aoc-2022/day-19/src/main.rs
@@ -11,8 +11,7 @@ struct State {
     robots: [u16; 4],
 }
 
-fn search(blueprint: &[[u16; 4]; 4]) -> u16 {
-    let max_time = 24;
+fn search(blueprint: &[[u16; 4]; 4], max_time: u16) -> u16 {
     let mut queue = Vec::new();
     queue.push(State {
         elapsed: 0,
@@ -20,6 +19,11 @@ fn search(blueprint: &[[u16; 4]; 4]) -> u16 {
         robots: [0, 0, 0, 1],
     });
     let mut seen = HashSet::new();
+
+    let mut max_robots = [u16::MAX; 4];
+    for i in 1..4 {
+        max_robots[i] = blueprint.iter().map(|cost| cost[i]).max().unwrap();
+    }
 
     let mut best = 0;
     while let Some(State {
@@ -31,6 +35,9 @@ fn search(blueprint: &[[u16; 4]; 4]) -> u16 {
         // Try to build each robot by waiting the required time for materials to be available (or skip if it can't be done in time).
         // If the robot is built, add a new state to the queue.
         for (i, costs) in blueprint.iter().rev().enumerate() {
+            if robots[i] == max_robots[i] {
+                continue;
+            }
             let need_to_wait = (1..4)
                 .map(|idx| {
                     match costs[idx] {
@@ -56,6 +63,17 @@ fn search(blueprint: &[[u16; 4]; 4]) -> u16 {
             let mut new_robots = robots;
             new_robots[i] += 1;
 
+            // Alpha pruning: If we built geode robots every turn from here on out and we still wouldn't beat the current best,
+            // then we can stop searching this branch. geode robots * remianing time + current geodes + sum(i for i in 0..remaining_time)
+            let remaining_time = max_time - elapsed - need_to_wait - 1;
+            if ((remaining_time - 1) * remaining_time) / 2
+                + new_materials[0]
+                + remaining_time * new_robots[0]
+                <= best
+            {
+                continue;
+            }
+
             let new_state = State {
                 elapsed: elapsed + need_to_wait + 1,
                 materials: new_materials,
@@ -74,9 +92,6 @@ fn search(blueprint: &[[u16; 4]; 4]) -> u16 {
 
 #[aocd(2022, 19)]
 fn main() {
-    //     let input = "Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
-    // Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.";
-
     let numbers = Regex::new(r"\d+").unwrap();
     let blueprints = input!()
         .lines()
@@ -96,10 +111,18 @@ fn main() {
 
     let part1: usize = blueprints
         .par_iter()
-        .map(|blueprint| search(blueprint))
+        .map(|blueprint| search(blueprint, 24))
         .enumerate()
         .map(|(idx, geodes)| (idx + 1) * (geodes as usize))
         .sum();
 
     submit!(1, part1);
+
+    let part2: usize = blueprints[0..3]
+        .par_iter()
+        .map(|blueprint| search(blueprint, 32))
+        .collect::<Vec<_>>()
+        .iter()
+        .fold(1usize, |a, b| a * usize::from(*b));
+    submit!(2, part2);
 }
